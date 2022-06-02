6228144c013b9ec483b49c733ab14850eafd15e7
Faraj11 <44307319+Faraj11@users.noreply.github.com>
2022-06-02 15:31:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,32 @@
-/**
- * This example can be found in the Processing examples package
- * that comes with the Processing PDE.
- * Processing > Examples > Topics > Interaction > Follow3
- * Adapted by Evelyn Eastmond
- */
+function setup ()
+{
+  background (#FFFFFF);
+  createCanvas (500, 500);
 
-var x = new Array(20);  // **change** float[] x = new float[20] to new Array(20)
-var y = new Array(20);  // **change** float[] y = new float[20] to new Array(20)
-var segLength = 18;                                 // **change** float to var
+  let x=random (500);
+  let y=random (500);
+  let z=random (100);
+  let a= random (100);
 
-function setup() {                          // **change** void setup() to function setup()
-  createCanvas(640, 360);                   // **change** size() to createCanvas()
-  strokeWeight(9);                          // strokeWeight() is the same
-  stroke(255, 100);                         // stroke() is the same
-  for(var i=0; i<x.length; i++) {         // initialize the array
-    x[i]=0;
-    y[i]=0;
+  let b= random (0, 256);
+  let c= random (0, 256);
+  let d= random (0, 256);
+
+  while (x < 500)
+  {
+    rectangle (x, y, z, a, b, c, d);
+    x=random (500);
+    y=random (500);
+    z=random (100);
+    a= random (100);
+
+    b= random (0, 250);
+    c= random (0, 250);
+    d= random (0, 250);
   }
 }
-
-function draw() {                           // **change** void draw() to function draw()
-  background(0);                            // background() is the same
-  drawSegment(0, mouseX, mouseY);           // functions calls, mouseX and mouseY are the same
-  for(var i=0; i<x.length-1; i++) {         // **change** int i to var i
-    drawSegment(i+1, x[i], y[i]);           // function calls are the same
-  }
-}
-
-function drawSegment(i, xin, yin) {         // **change** void drawSegment() to function drawSegment(), remove type declarations
-  var dx = xin - x[i];                      // **change** float to var
-  var dy = yin - y[i];                      // **change** float to var
-  var angle = atan2(dy, dx);                // **change** float to var, atan2() is the same
-  x[i] = xin - cos(angle) * segLength;      // cos() is the same
-  y[i] = yin - sin(angle) * segLength;      // sin() is the same
-  segment(x[i], y[i], angle);               // function calls are the same
-}
-
-function segment(x, y, a) {                 // **change** void segment() to function segment(), remove type declarations
-  push();                            		// pushMatrix() becomes push()
-  translate(x, y);                          // translate() is the same
-  rotate(a);                                // rotate() is the same
-  line(0, 0, segLength, 0);                 // line() is the same
-  pop();                              		// popMatrix() becomes pop()
+function rectangle (x, y, z, a, b, c, d)
+{
+  fill (b, c, d);
+  rect (x, y, z, a);
 }
