654cada0be30e2b4c82d5b8fb3a9fbf91e9361de
Faraj11 <44307319+Faraj11@users.noreply.github.com>
2022-06-02 19:46:12 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,71 @@
-function setup ()
-{
-  background (0);
-  createCanvas (500, 500);
-
-  let x=random (500);
-  let y=random (500);
-  let z=random (100);
-  let a= random (100);
-
-  let b= random (0, 256);
-  let c= random (0, 256);
-  let d= random (0, 256);
-
-  while (x < 500)
-  {
-    rectangle (x, y, z, a, b, c, d);
-    x=random (500);
-    y=random (500);
-    z=random (100);
-    a= random (100);
-
-    b= random (0, 250);
-    c= random (0, 250);
-    d= random (0, 250);
-  }
+let s;
+let m;
+let h;
+let x;
+function setup() {
+  size (400, 400);
+  createCanvas(255);
+   s=second();
+   m=minute();
+   h=hour();
 }
-function rectangle (x, y, z, a, b, c, d)
+
+function draw() {
+  background(0);
+  clockFace();
+  drawHand();
+  updateTime();
+  
+}
+
+void timeToAngle ()
 {
-  fill (b, c, d);
-  rect (x, y, z, a);
+
+  let s = map(second(), 0, 60, 0, (PI*2)) - (PI/2);
+  
+  stroke(255,0,0);
+  strokeWeight(1);
+  line(width/2, height/2, width/2 + (115 * (cos(s))), height/2 + (115 * (sin(s))));
+
+  let m = map(minute(), 0, 60, 0, (PI * 2)) - (PI/2); 
+  
+  stroke(0,0,0);
+  strokeWeight(3);
+  line(width/2, height/2, width/2 + (85 * (cos(m))), height/2 + (85 * (sin(m))));
+
+  let h = map(hour(), 0, 24, 0, (PI*2) * 2) - (PI/2);
+  
+  stroke(0,0,0);
+  strokeWeight(6);
+  line(width/2, height/2, width/2 + (55 * (cos(h))), height/2 + (55 * (sin(h))));
+}
+
+function drawHand () 
+{
+  timeToAngle();
+}
+
+function clockFace () {
+  stroke(0, 0, 0);
+  strokeWeight(1);
+  fill (120,120,200);
+  circle(width/2, height/2, 300);
+  
+  beginShape(POINTS);
+  strokeWeight(5);
+   x=0;
+  for (int i = 0; i < 360; i+=30) {
+  
+    let t = map(i,0,360,0,(2*PI));
+    let x = 200+ cos(t) * 130;          
+    let y = 200+ sin(t) * 130;          
+    vertex(x, y);
+  }
+  endShape();
+}
+
+function updateTime () {
+  let s=second();
+  let m=minute();
+  let h=hour();
 }
