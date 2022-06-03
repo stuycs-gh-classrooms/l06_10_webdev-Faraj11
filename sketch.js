b8e560c584a0bef65adebd0dfcf69e162ffc88f2
Faraj11 <44307319+Faraj11@users.noreply.github.com>
2022-06-02 20:06:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,6 @@ let h;
 let x;
 function setup() {
   createCanvas (400, 400);
-//  background (255);
   s=second();
   m=minute();
   h=hour();
@@ -48,7 +47,7 @@ function clockFace () {
   stroke(0, 0, 0);
   strokeWeight(1);
   fill (120,120,200);
-  circle(width/2, height/2, 300);
+  circle(0, 0, 300);
 
   beginShape(POINTS);
   strokeWeight(5);
